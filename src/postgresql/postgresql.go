424fd33bbbdfd313e415e109129e4c7cdbb3cf8d
Celtcoste <kylian.colson@epitech.eu>
2020-06-07 19:52:25 +0200

    Checking env var for splitting DB Port

--- src/postgresql/postgresql.go
+++ src/postgresql/postgresql.go
@@ -6,6 +6,8 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 	"log"
+	"net"
+	"os"
 )
 
 var PostgresConn *sql.DB
@@ -20,7 +22,9 @@ type Transaction interface {
 type TxFn func(Transaction) error
 
 func Setup(host string, port string, user string, password string, databasename string) error {
-	//host, port, _ = net.SplitHostPort(host)
+	if os.Getenv("APP_ENV") == "PROD" {
+		host, port, _ = net.SplitHostPort(host)
+	}
 
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable",
